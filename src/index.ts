890d3e24422bace21da43a1085976249d5a98887
0xshortcake <0xshortcake@gmail.com>
2022-07-29 14:58:48 -0700

    feat: add tc
    Release-As: 1.2.0

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
+import { Erc20__factory } from './contracts';
+
 export const sum = (a: number, b: number) => {
   if ('development' === process.env.NODE_ENV) {
     console.log('sum boop');
   }
+  console.log(Erc20__factory);
   return a + b;
 };
 
